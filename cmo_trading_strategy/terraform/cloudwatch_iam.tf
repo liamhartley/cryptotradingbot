0d251bd4b700a8b19cf8d98c23eb8028eafae60e
liamhartley <liamjhartley@yahoo.co.uk>
2021-05-24 17:23:25 +0100

    increasing frequency

---
+++ cmo_trading_strategy/terraform/cloudwatch_iam.tf
@@ -0,0 +1,7 @@
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_cmo_strategy" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.cryptotradingbot_cmo.function_name
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.every_hour.arn
+}
