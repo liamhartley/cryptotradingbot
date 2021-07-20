d5ecc9ba039837b7da1147aa84e6019fbad2391a
liamhartley <liamjhartley@yahoo.co.uk>
2021-07-20 09:37:22 +0100

    working on bugs: https://www.youtube.com/watch?v=ee0JCfeFw1o

--- trading_strategies/poloniex_cmo_trading_strategy/config.py
+++ trading_strategies/poloniex_cmo_trading_strategy/config.py
@@ -2,7 +2,7 @@ LOGICAL_PARAMS = {
     "CMO_PERIOD": 10,
     "PERIOD": 86400,
     "PAIR": "BTC_XRP",
-    "OVERSOLD_VALUE": -50,
+    "OVERSOLD_VALUE": -60,
     "OVERBOUGHT_VALUE": 50,
     "DRY_RUN": False,
     "INITIAL_CAPITAL": 0.018,  # in quote currency
