8daeff0565d30c239840ff6a18d05ed5506b481b
liamhartley <liamjhartley@yahoo.co.uk>
2021-08-22 14:01:46 +0100

    reducing overbought threshold

--- trading_strategies/poloniex_cmo_trading_strategy/config.py
+++ trading_strategies/poloniex_cmo_trading_strategy/config.py
@@ -3,7 +3,7 @@ LOGICAL_PARAMS = {
     "PERIOD": 86400,
     "PAIR": "BTC_XRP",
     "OVERSOLD_VALUE": -60,
-    "OVERBOUGHT_VALUE": 50,
+    "OVERBOUGHT_VALUE": 39,
     "DRY_RUN": False,
     "INITIAL_CAPITAL": 0.018,  # in quote currency
     "ENTRY_SIZE": 0.1  # 10%
